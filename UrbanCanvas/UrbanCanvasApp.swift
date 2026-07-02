972b644ec84d1f32bc5a03eeea52da4578ed1f2e
Mathieu <mathieu.rolland9@gmail.com>
2026-07-02 13:24:03 +0200

    Initial Commit

---
+++ UrbanCanvas/UrbanCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanCanvasApp.swift
+//  UrbanCanvas
+//
+//  Created by Apprenant 77 on 02/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct UrbanCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
