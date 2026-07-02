c6ec70993fe098672844d782368153532e8cd111
Mathieu <mathieu.rolland9@gmail.com>
2026-07-02 16:30:22 +0200

    linked data, extracted views

---
+++ UrbanCanvas/Components/ArtworkListElementView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkListElementView.swift
+//  UrbanCanvas
+//
+//  Created by Apprenant 77 on 02/07/2026.
+//
+
+import SwiftUI
+
+struct ArtworkListElementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtworkListElementView()
+}
