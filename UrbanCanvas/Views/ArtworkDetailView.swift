e240f53e71837618af8eff6b7feb6361b7e9a3f2
Mathieu <mathieu.rolland9@gmail.com>
2026-07-02 14:08:03 +0200

    scaffolding done, need to pass data

---
+++ UrbanCanvas/Views/ArtworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkDetailView.swift
+//  UrbanCanvas
+//
+//  Created by Apprenant 77 on 02/07/2026.
+//
+
+import SwiftUI
+
+struct ArtworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtworkDetailView()
+}
---
+++ UrbanCanvas/Views/ArtworkListView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkListView.swift
+//  UrbanCanvas
+//
+//  Created by Apprenant 77 on 02/07/2026.
+//
+
+import SwiftUI
+
+struct ArtworkListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtworkListView()
+}
