7026de776f5a65a13665c89eddc1c889779f1d9b
Mathieu <mathieu.rolland9@gmail.com>
2026-07-03 11:48:11 +0200

    added menu, working on filtering artworks by style

---
+++ UrbanCanvas/Components/FilterMenuButton.swift
@@ -0,0 +1,18 @@
+//
+//  FilterMenuButton.swift
+//  UrbanCanvas
+//
+//  Created by Apprenant 77 on 03/07/2026.
+//
+
+import SwiftUI
+
+struct FilterMenuButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterMenuButton()
+}
---
+++ UrbanCanvas/Views/PickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerView.swift
+//  UrbanCanvas
+//
+//  Created by Apprenant 77 on 03/07/2026.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerView()
+}
