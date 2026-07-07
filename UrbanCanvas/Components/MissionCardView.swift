8051a7fbbb3e2ed7ba1821b198025b715b8fef98
Mathieu <mathieu.rolland9@gmail.com>
2026-07-07 10:24:25 +0200

    working on mission feature

--- UrbanCanvas/Components/MissionCardView.swift
+++ UrbanCanvas/Components/MissionCardView.swift
@@ -2,20 +2,65 @@ import SwiftUI
 
 struct MissionCardView: View {
     let artwork :Artwork
+    let missionIndex :Int
+    @State private var isMissionComplete :Bool = false
+    @Environment(ArtworksList.self) private var artworkList
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Mission :")
-            Image(artwork.imageName)
-                .resizable()
-                .scaledToFit()
-                .frame(minWidth: 200, maxHeight: 200)
-            Text(artwork.name)
-            
-            
+        NavigationStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Mission \(missionIndex) :")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.mainOrange)
+                    Spacer()
+                    NavigationLink {
+                        ArtworkDetailView(artwork: artworks[1])
+                    } label: {
+                        Image(systemName: "arrow.right.circle.fill")
+                            .circleImage(frameSize: 50)
+                        .foregroundStyle(.mainOrange)                    }
+                }
+                Image(artwork.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(minWidth: 200, maxHeight: 200)
+                    .cornerRadius(10)
+                HStack {
+                    Text(artwork.name)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.mainOrange)
+                    Spacer()
+                    Button {
+                        isMissionComplete.toggle()
+                        if isMissionComplete {
+                            artworkList.currentMission.remove(at: missionIndex)
+                        }
+                    } label: {
+                        Image(systemName: "checkmark.circle.fill")
+                            .circleImage(frameSize: 30)
+                            .foregroundStyle(isMissionComplete ? .secondOrange : .white)
+                            .shadow(radius: 5)
+                    }
+                }
+                Text("\(Text("Type :").bold()) \(artwork.artType)")
+                Text("\(Text("Auteur :").bold()) \(artwork.author.name)")
+                Text("\(Text("Localisation :").bold()) \(artwork.localisation)")
+            }
+            .padding()
+            .background(
+                Rectangle()
+                    .foregroundStyle(.backgroundGray)
+                    .cornerRadius(10)
+                
+            )
         }
+        .padding(50)
     }
 }
 
 #Preview {
-    MissionCardView(artwork: artworks[2])
+    MissionCardView(artwork: artworks[2], missionIndex: 2)
+        .environment(ArtworksList())
 }
--- UrbanCanvas/Views/MissionView.swift
+++ UrbanCanvas/Views/MissionView.swift
@@ -3,30 +3,43 @@ import SwiftUI
 struct MissionView: View {
     @State var show = false
     @Environment(ArtworksList.self) private var artworkList
-    var missionArray = [1,2,3,4,5]
-
+    
+    
     var body: some View {
-        NavigationStack {
-            ForEach (artworkList.missionArray) { mission in
-                ArtworkListElementView(artwork: mission)
-        }
-        }
-        .navigationTitle("Mission")
-        Button {
-            withAnimation(.spring().delay(0.5)) {
-                show.toggle()
+        ZStack {
+            if artworkList.currentMission.count == 0 {
+                Button {
+                    if artworkList.currentMission.count == 0 {
+                        artworkList.currentMission = artworkList.createMission()
+                    }
+                }
+                label: {
+                    Text("Nouvelle mission")
+                        .font(.title)
+                        .bold()
+                        .foregroundStyle(.mainText)
+                        .padding()
+                        .frame(maxWidth:.infinity)
+                        .background(
+                            Circle()
+                                .scaledToFill()
+                                .foregroundStyle(.secondOrange)
+                        )
+                }
+                .padding()
             }
-        } label: {
-            Text("Nouvelle mission")
-                .foregroundStyle(.accent)
-            .padding()
-            .frame(maxWidth:.infinity)
-            .background(
-                Capsule()
-                    .foregroundStyle(.backgroundGray)
-            )
-    }
-        .padding()
+            else {
+                NavigationStack {
+                    ScrollView {
+                        VStack {
+                            ForEach(artworkList.currentMission.enumerated(), id: \.element.id) { index, mission in
+                                MissionCardView(artwork: mission, missionIndex: (index))
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
